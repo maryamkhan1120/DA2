491d7c801a12a6870382b98b01e0a22e44e21002
maryamkhan1120 <90628903+maryamkhan1120@users.noreply.github.com>
2021-11-24 12:37:19 +0100

    version3: Filtered the data

--- Assignment1/DA2-Assignment1.R
+++ Assignment1/DA2-Assignment1.R
@@ -28,3 +28,18 @@ janitor::tabyl(data_all$state)
 data <- data_all %>% filter(occ2012==0840)
 
 tabulate(data_all$sample)
+
+#gen female variable
+#gen wage variables
+data <- data %>% mutate(female=as.numeric(sex==2)) %>%
+  mutate(w=earnwke/uhours) %>%
+  mutate(lnw=log(w)) %>%
+  mutate(agesq=age^2)
+
+#SET SAMPLE - Choose one of the occupations!
+#Market research analysts and marketing specialists -1
+#Computer and Mathematical Occupations-2
+
+
+write_csv(data,paste("/Users/maryamkhan/Data_Analysis2/","earnings_inference.csv",sep=""))
+
