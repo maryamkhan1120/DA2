e26ef739dab7f30123f1e357ebf80f133291418c
maryamkhan1120 <90628903+maryamkhan1120@users.noreply.github.com>
2021-11-24 12:34:38 +0100

    version2: Loading the data

--- Assignment1/DA2-Assignment1.R
+++ Assignment1/DA2-Assignment1.R
@@ -15,3 +15,16 @@ install.packages("estimatr")
 install.packages("huxtable")
 install.packages("stargazer")
 getwd()
+
+#import data (state must be as character: it's a mix of double and character in raw)
+data_all <- read_csv(paste0("/Users/maryamkhan/Data_Analysis2/morg-2014-emp.csv"), 
+                     col_types = cols(.default = "?", 
+                                      state = "c"))
+janitor::tabyl(data_all$state)
+
+#SELECT OCCUPATION
+# keep only two occupation types: Market research analysts and marketing specialists 
+#and Computer and Mathematical Occupations
+data <- data_all %>% filter(occ2012==0840)
+
+tabulate(data_all$sample)
